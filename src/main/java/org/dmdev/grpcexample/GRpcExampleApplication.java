4c39071e9884fccd263c52fa955b39d7a8b3d3cb
Dmytro Matvieiev <matvievievie@gmail.com>
2024-03-27 13:14:49 +0200

    initial commit

---
+++ src/main/java/org/dmdev/grpcexample/GRpcExampleApplication.java
@@ -0,0 +1,13 @@
+package org.dmdev.grpcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dmdev/grpcexample/GRpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmdev.grpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
